3babb659d834d3e7b9c92bb39f1e777c91d8f63f
PacteraWangCong <cong.wang3@pactera.com>
2020-11-23 21:41:04 +0800

    FEAT: MyObserver 追加

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,31 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       initialRoute: AppRoutes.home,
       getPages: AppRoutes.routes,
+      navigatorObservers: [
+        MyObserver()
+      ],
     );
+  }
+}
+
+/// 监听路由发生变化
+class MyObserver extends NavigatorObserver {
+
+  @override
+  void didPush(Route route, Route previousRoute) {
+    super.didPush(route, previousRoute);
+    if (route is MaterialPageRoute || route is GetPageRoute) {
+      //appBloc.routeChange(RouteChangeType.push);
+      print('route is didPush');
+    }
+  }
+
+  @override
+  void didPop(Route route, Route previousRoute) {
+    super.didPop(route, previousRoute);
+    if (route is MaterialPageRoute || route is GetPageRoute) {
+      //appBloc.routeChange(RouteChangeType.pop);
+      print('route is didPop');
+    }
   }
 }
